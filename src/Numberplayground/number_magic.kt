87ae24f27b0f02e99b6a482fb90afd3d72360c37
Njood <nejood03@gmail.com>
2025-02-20 16:15:46 +0300

    step 2 and 3 are complete

--- src/Numberplayground/number_magic.kt
+++ src/Numberplayground/number_magic.kt
@@ -1,9 +1,14 @@
 package Numberplayground
 
 fun main() {
+    val numbers = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
     countUpwards(1, 10)
     countDownwards(10, 1)
+    val sum = calculateSum(numbers)
+    println(sum)
 }
+
 
 fun countUpwards(start: Int, end: Int) {
     for (i in start..end) {
@@ -18,3 +23,12 @@ fun countDownwards(end: Int, start: Int) {
         i--
     }
 }
+
+fun calculateSum(numbers: IntArray): Int {
+    var sum = 0
+    for (num in numbers) {
+        sum += num
+    }
+    return sum
+}
+
